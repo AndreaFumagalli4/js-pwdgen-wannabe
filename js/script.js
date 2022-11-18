dfb978739225a64728f5de5f66eea5b3cc2df9f8
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-18 15:43:20 +0100

    add older age / bonus

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 // Ask user first name
 
-let firstName = prompt ('What\' your first name?');
+const firstName = prompt ('What\' your first name?');
 
 console.log (firstName);
 
 // Ask user second name
 
-let secondName = prompt ('What\' your second name?');
+const secondName = prompt ('What\' your second name?');
 
 console.log (secondName);
 
 // Ask user favorite color
 
-let favoriteColor = prompt ('What\' your favorite color?');
+const favoriteColor = prompt ('What\' your favorite color?');
 
 console.log (favoriteColor);
 
+// Ask user age 
+
+const ageNow = parseInt ( prompt ('How old are you?') );
+
+const addAge = 10;
+
+const realAge = ageNow + addAge;
+
+console.log (realAge);
+
 // Write HTML questions result
 
-document.getElementById ('output').innerHTML = firstName + secondName + favoriteColor + '22';
+document.getElementById ('output').innerHTML = firstName + secondName + favoriteColor + realAge;
