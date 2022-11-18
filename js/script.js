809077423be4bead0bba4a7beef90ae5f518d9f7
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-18 14:54:47 +0100

    ask to user first and second name

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Ask user first name
+
+let firstName = prompt ('What\' your first name?');
+
+console.log (firstName);
+
+// Ask user second name
+
+let secondName = prompt ('What\' your second name?');
+
+console.log (secondName);
+
